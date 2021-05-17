90f95b7e9a87154c46a6294edf19319b68c3079b
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-17 16:01:28 +0100

    initial commit

---
+++ src/main/java/com/BeedleElsmie/CharacterBuilder/Models/Classes.java
@@ -0,0 +1,48 @@
+package com.BeedleElsmie.CharacterBuilder.Models;
+
+public class Classes {
+
+    private Long id;
+    private String name;
+    private String index;
+    private String url;
+
+    public Classes(Long id, String name, String index, String url) {
+        this.id = id;
+        this.name = name;
+        this.index = index;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/test/java/com/BeedleElsmie/CharacterBuilder/CharacterBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeedleElsmie.CharacterBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
