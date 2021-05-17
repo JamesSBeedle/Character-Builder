93391eacbe94b93840109018c5b228b83980e62f
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-17 18:19:05 +0100

    added save function to data loader and used h2 to check database seeding had worked

--- src/main/java/com/BeedleElsmie/CharacterBuilder/Components/DataLoader.java
+++ src/main/java/com/BeedleElsmie/CharacterBuilder/Components/DataLoader.java
@@ -36,21 +36,33 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) {
 
         Equipment equipment1 = new Equipment("Long Sword", "longsword", "longsword.com");
+        equipmentRepository.save(equipment1);
         Equipment equipment2 = new Equipment("Short Sword", "shortsword", "shortsword.com");
+        equipmentRepository.save(equipment2);
 
         Feat feat1 = new Feat("Dual Wielding", "dualwield", "dualwield.com");
+        featRepository.save(feat1);
         Feat feat2 = new Feat("Bash", "bash", "bash.com");
+        featRepository.save(feat2);
 
         Race race1 = new Race("Dwaf", "dwarf", "dwarf.com");
+        raceRepository.save(race1);
         Race race2 = new Race("Elf", "elf", "elf.com");
-        
+        raceRepository.save(race2);
+
         Role role1 = new Role("Cleric", "cleric", "cleric.com");
+        roleRepository.save(role1);
         Role role2 = new Role("Fighter", "fighter", "fighter.com");
+        roleRepository.save(role2);
 
         Skill skill1 = new Skill("Acrobatics", "acrobatics", "acrobatics.com");
+        skillRepository.save(skill1);
         Skill skill2 = new Skill("Climb", "climb", "climb.com");
+        skillRepository.save(skill2);
 
         Spell spell1 = new Spell ("Magic Missile", "magic missile", "magicmissile.com");
+        spellRepository.save(spell1);
         Spell spell2 = new Spell("Eldritch Blast", "eldritch blast", "eldritchblast.com");
+        spellRepository.save(spell2);
     }
 }
