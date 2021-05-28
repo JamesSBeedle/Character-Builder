22c1896cf7ea064dc4be110d810e5958f172730c
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-28 11:22:21 +0100

    added further columns to the proficiency model

--- src/main/java/com/BeedleElsmie/CharacterBuilder/Models/Proficiency.java
+++ src/main/java/com/BeedleElsmie/CharacterBuilder/Models/Proficiency.java
@@ -2,6 +2,7 @@ package com.BeedleElsmie.CharacterBuilder.Models;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Table(name="proficiencies")
@@ -14,8 +15,20 @@ public class Proficiency {
     @Column(name = "name")
     private String name;
 
-    public Proficiency(String name) {
+    @Column(name ="type")
+    private String type;
+
+    @Column(name ="classes")
+    private ArrayList<Role> classes;
+
+    @Column(name = "races")
+    private ArrayList<Race> races;
+
+    public Proficiency(String name, String type, ArrayList<Role> classes, ArrayList<Race> races) {
         this.name = name;
+        this.type = type;
+        this.classes = classes;
+        this.races = races;
     }
 
     public Proficiency() {
@@ -35,5 +48,29 @@ public class Proficiency {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Role> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<Role> classes) {
+        this.classes = classes;
+    }
+
+    public ArrayList<Race> getRaces() {
+        return races;
+    }
+
+    public void setRaces(ArrayList<Race> races) {
+        this.races = races;
     }
 }
