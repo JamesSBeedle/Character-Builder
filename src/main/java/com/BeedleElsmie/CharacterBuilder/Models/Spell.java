d91034d44ef2aab4f69f4d7268f29599fad4977a
JamesSBeedle <jamessbeedle@gmail.com>
2021-05-28 11:03:48 +0100

    added more table columns to the spell model

--- src/main/java/com/BeedleElsmie/CharacterBuilder/Models/Spell.java
+++ src/main/java/com/BeedleElsmie/CharacterBuilder/Models/Spell.java
@@ -13,16 +13,68 @@ public class Spell {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "index")
-    private String index;
+    @Column(name = "description")
+    private String description;
 
-    @Column(name = "url")
-    private String url;
+    @Column(name = "higher-level")
+    private String higherLevel;
 
-    public Spell(String name, String index, String url) {
+    @Column(name = "range")
+    private int range;
+
+    @Column(name = "components")
+    private String components;
+
+    @Column(name ="materials")
+    private String materials;
+
+    @Column(name = "ritual")
+    private boolean ritual;
+
+    @Column(name = "duration")
+    private int duration;
+
+    @Column(name = "casting-time")
+    private String castingTime;
+
+    @Column(name = "concentration")
+    private boolean concentration;
+
+    @Column(name = "attack-type")
+    private String attackType;
+
+    @Column(name = "damage")
+    private Damage damage;
+
+    @Column(name = "school")
+    private String school;
+
+    @Column(name = "class")
+    private ArrayList<Role> class;
+
+    @Column(name = "sub-class")
+    private ArrayList<SubClass> subClass;
+
+    @Column(name = "level")
+    private int level;
+
+
+    public Spell(String name, String description, String higherLevel, int range, String components, String materials, boolean ritual, int duration, String castingTime, boolean concentration, String attackType, Damage damage, String school, ArrayList<SubClass> subClass, int level) {
         this.name = name;
-        this.index = index;
-        this.url = url;
+        this.description = description;
+        this.higherLevel = higherLevel;
+        this.range = range;
+        this.components = components;
+        this.materials = materials;
+        this.ritual = ritual;
+        this.duration = duration;
+        this.castingTime = castingTime;
+        this.concentration = concentration;
+        this.attackType = attackType;
+        this.damage = damage;
+        this.school = school;
+        this.subClass = subClass;
+        this.level = level;
     }
 
     public Spell() {
@@ -45,19 +97,115 @@ public class Spell {
         this.name = name;
     }
 
-    public String getIndex() {
-        return index;
+    public String getDescription() {
+        return description;
     }
 
-    public void setIndex(String index) {
-        this.index = index;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getUrl() {
-        return url;
+    public String getHigherLevel() {
+        return higherLevel;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public void setHigherLevel(String higherLevel) {
+        this.higherLevel = higherLevel;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public String getComponents() {
+        return components;
+    }
+
+    public void setComponents(String components) {
+        this.components = components;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(String materials) {
+        this.materials = materials;
+    }
+
+    public boolean isRitual() {
+        return ritual;
+    }
+
+    public void setRitual(boolean ritual) {
+        this.ritual = ritual;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getCastingTime() {
+        return castingTime;
+    }
+
+    public void setCastingTime(String castingTime) {
+        this.castingTime = castingTime;
+    }
+
+    public boolean isConcentration() {
+        return concentration;
+    }
+
+    public void setConcentration(boolean concentration) {
+        this.concentration = concentration;
+    }
+
+    public String getAttackType() {
+        return attackType;
+    }
+
+    public void setAttackType(String attackType) {
+        this.attackType = attackType;
+    }
+
+    public Damage getDamage() {
+        return damage;
+    }
+
+    public void setDamage(Damage damage) {
+        this.damage = damage;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public ArrayList<SubClass> getSubClass() {
+        return subClass;
+    }
+
+    public void setSubClass(ArrayList<SubClass> subClass) {
+        this.subClass = subClass;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 }
